c1e17845faa5ddcbb53f1d9d3d0e7c7c576534fb
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-09-15 01:29:16 +0530

    Update zipfile.py

--- stdplugins/zipfile.py
+++ stdplugins/zipfile.py
@@ -38,7 +38,7 @@ async def _(event):
     await borg.send_file(
         event.chat_id,
         directory_name + ".zip",
-        caption="Zipped By SnapDragon",
+        caption="Zipped By EyePatch",
         force_document=True,
         allow_cache=False,
         reply_to=event.message.id,
