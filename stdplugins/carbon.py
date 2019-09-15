76236295c3feacc18d72014cdd412e90e19297be
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-09-15 14:22:05 +0530

    Update carbon.py

--- stdplugins/carbon.py
+++ stdplugins/carbon.py
@@ -2,7 +2,7 @@
 Carbon Scraper Plugin for Userbot. //text in creative way.
 usage: .carbon //as a reply to any text message
 
-Thanks to @r4v4n4 for vars
+Thanks to @NeoMatrix90 for vars
 
 """
 from selenium.webdriver.support.ui import Select
@@ -62,7 +62,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Carbon by [@r4v4n4](https://www.github.com/ravana69/pornhub)/",
+         caption="Carbon by [@NeoMatrix90](https://t.me/NeoMatrix90)/",
          force_document=True,
          reply_to=e.message.reply_to_msg_id,
          )
