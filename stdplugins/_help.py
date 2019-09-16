b6c3f25abe2cd533c9464e245cc69b81768cb2ff
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-09-16 10:52:03 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -64,7 +64,7 @@ async def _(event):
     plugin_name = event.pattern_match.group(1)
     if plugin_name in borg._plugins:
         help_string = borg._plugins[plugin_name].__doc__
-        unload_string = f"Use `.unload {plugin_name}` to remove this plugin.\n           © kirito6969"
+        unload_string = f"Use `.unload {plugin_name}` to remove this plugin.\n           © @NeoMatrix90 @kirito6969"
         if help_string:
             plugin_syntax = f"Syntax for plugin **{plugin_name}**:\n\n{help_string}\n{unload_string}"
         else:
