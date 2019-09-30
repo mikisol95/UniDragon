91244b89518bc7a7edb4e5175b637b0d90cd5cc2
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-09-30 13:42:36 +0530

    Edit ggl

--- stdplugins/howtogoogle.py
+++ stdplugins/howtogoogle.py
@@ -20,6 +20,6 @@ async def _(event):
     sample_url = "https://da.gd/s?url=https://lmgtfy.com/?q={}%26iie=1".format(input_str.replace(" ","+"))
     response_api = requests.get(sample_url).text
     if response_api:
-        await event.edit("[{}]({})\n`Thank me Later 🙃` ".format(input_str,response_api.rstrip()))
+        await event.edit("[{}]({})\n`Le Link aur dalde apne piche 🖕` ".format(input_str,response_api.rstrip()))
     else:
         await event.edit("something is wrong. please try again later.")
