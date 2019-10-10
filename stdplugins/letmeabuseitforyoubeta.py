e447c5571615aeb337deb2630ab9caa0526654d7
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-10-10 16:28:19 +0530

    Update x

--- stdplugins/letmeabuseitforyoubeta.py
+++ stdplugins/letmeabuseitforyoubeta.py
@@ -7,7 +7,7 @@ from telethon import events
 import random
 import asyncio
 
-@borg.on(events.NewMessage(pattern=r"\.g(.*)", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.x(.*)", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
