ab2217852784fb55b3808268ba15a91bff997886
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-10-13 03:06:34 +0530

    Okkkkk

--- stdplugins/screencapture.py
+++ stdplugins/screencapture.py
@@ -1,5 +1,5 @@
 """Take screenshot of any website
-Syntax: .screencapture <Website URL>"""
+Syntax: .cap <Website URL>"""
 
 import io
 import requests
@@ -7,7 +7,7 @@ from telethon import events
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("screencapture (.*)"))
+@borg.on(admin_cmd("cap (.*)"))
 async def _(event):
     if event.fwd_from:
         return
