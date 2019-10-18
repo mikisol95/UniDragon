81c19cd37fc9cd7633d9ac3f8cb3ded561a39795
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-10-18 10:45:00 +0530

    Update moon.py

--- stdplugins/moon.py
+++ stdplugins/moon.py
@@ -109,5 +109,5 @@ async def _(event):
 
             await asyncio.sleep(animation_interval)
 
-            await event.edit(animation_chars[i % 117])
+            await event.edit(animation_chars[i % 32])
 
