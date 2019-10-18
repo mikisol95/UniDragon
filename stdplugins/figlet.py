10bcbe44daa67ae8ba912a880cc6f6f269f98dec
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-10-18 14:38:00 +0000

    Update figlet.py

--- stdplugins/figlet.py
+++ stdplugins/figlet.py
@@ -8,7 +8,7 @@ from telethon import events, functions
 from uniborg.util import admin_cmd
 import sys
  
-@borg.on(admin_cmd(pattern="figlet ?(.*)", allow_sudo=True))  # pylint:disable=E0602
+@borg.on(admin_cmd(pattern="figlet ?(.*)", allow_sudo=True))
 async def figlet(event):
     if event.fwd_from:
         return
