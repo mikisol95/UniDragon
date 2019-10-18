d4dad8f56ac2f32ad3f0204f893ed7191c7795cc
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-10-18 09:12:11 +0530

    Update F.py

--- stdplugins/F.py
+++ stdplugins/F.py
@@ -5,7 +5,7 @@ import sys
 from uniborg import util
 
 
-@borg.on(util.admin_cmd(pattern="ftext ?(.*)"))
+@borg.on(util.admin_cmd(pattern="ft ?(.*)"))
 async def payf(event):
     paytext=event.pattern_match.group(1)
     pay = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}".format(paytext*8, paytext*8, paytext*2, paytext*2, paytext*2, paytext*6, paytext*6, paytext*2, paytext*2, paytext*2, paytext*2, paytext*2)
