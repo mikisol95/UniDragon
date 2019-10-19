ed3f727264ba562607fe8683bf0413a72e10df1f
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-10-19 05:12:59 +0000

    Kanging

--- stdplugins/kang.py
+++ stdplugins/kang.py
@@ -137,7 +137,7 @@ async def _(event):
             await silently_send_message(bot_conv, sticker_emoji)
             await silently_send_message(bot_conv, "/done")
 
-    await event.edit(f"`This Sticker Is Raped! Plox Help this Sticker by Clicking` [Here](t.me/addstickers/{packshortname})")
+    await event.edit(f"`This Sticker Is Raped! Plox Help this Sticker by Clicking` [HERE](t.me/addstickers/{packshortname})")
 
 
 @borg.on(admin_cmd("packinfo"))
