05f4f85c049ad9bb5f1f3ea90e4177647d277372
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-10-19 15:29:44 +0000

    Update

--- stdplugins/Kill.py
+++ stdplugins/Kill.py
@@ -19,7 +19,7 @@ async def _(event):
     animation_interval = 30
     
 
-    animation_ttl = range(0, 103)
+    animation_ttl = range(0, 100)
 
     input_str = event.pattern_match.group(1)
 
@@ -46,4 +46,4 @@ async def _(event):
         for i in animation_ttl:
 
 
-            await event.edit(animation_chars[i % 10])
+            await event.edit(animation_chars[i % 100])
