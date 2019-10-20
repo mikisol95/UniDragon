b2d7b3e885d72cf184033a2fe808e591c06a9d11
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-10-20 06:51:36 +0000

    Okaytg

--- stdplugins/letmeabuseitforyoubeta.py
+++ stdplugins/letmeabuseitforyoubeta.py
@@ -1,4 +1,4 @@
-"""command: .ohin , .omal"""
+"""command: .jhin , .jmal"""
 
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
@@ -7,7 +7,7 @@ from telethon import events
 import random
 import asyncio
 
-@borg.on(events.NewMessage(pattern=r"\.o(.*)", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.j(.*)", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
