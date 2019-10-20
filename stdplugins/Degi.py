2a67907e600f73b311815d9110d97f9549128f54
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-10-20 02:42:50 +0000

    Uploaded New Plugin

---
+++ stdplugins/Degi.py
@@ -0,0 +1,32 @@
+"""Fun pligon...
+\nCode by @kirito6969
+type .degi and ...
+"""
+
+from telethon import events
+import random, re
+from uniborg.util import admin_cmd
+import asyncio 
+
+
+
+@borg.on(admin_cmd("degi ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("Wo")
+        await asyncio.sleep(0.7)
+        await event.edit("Degi")
+        await asyncio.sleep(1)
+        await event.edit("Tum")
+        await asyncio.sleep(0.8)
+        await event.edit("Ekbar")
+        await asyncio.sleep(0.9)
+        await event.edit("Mang")
+        await asyncio.sleep(1)
+        await event.edit("Kar")
+        await asyncio.sleep(0.8)
+        await event.edit("Toh")
+        await asyncio.sleep(0.7)
+        await event.edit("Dekho")
+        await asyncio.sleep(1)
+        await event.edit("`Wo Degi Tum Ekbar Mang Kar toh Dekho`")
