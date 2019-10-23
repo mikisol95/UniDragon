f81bd61d8caf2149e4de31ff32c8ff4089d498c8
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-10-24 02:23:59 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -14,11 +14,11 @@ async def _(event):
         s_help_string = borg._plugins[splugin_name].__doc__
     else:
         s_help_string = "****:"
-    help_string = """@UniBorg ( **Custom Built By** @NeoMatrix90 ) \n**Verified Account**: ✅\n**DESCRIPTION**: [PEPE IZ LUB](https://telegra.ph/file/5b5388c1a96670bd96b5e.png)\n
+    help_string = """@UniBorg ( **Custom Built By** @NeoMatrix90 ) \n**Verified Account**: ✅\n**DESCRIPTION**: **Magical Pepe Will Guide You.Touch the Link.**\n
 Pithun {}
 Talethrun {}
  
-**Custom Built Fork**: github.com/prono69/PepeBot""".format(
+**Custom Built Fork**: https://github.com/prono69/PepeBot""".format(
         sys.version,
         __version__
     )
