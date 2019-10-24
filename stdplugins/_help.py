92ec67a4d08369fbacedb543a4268f90e89ac57c
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-10-24 09:46:26 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -14,7 +14,7 @@ async def _(event):
         s_help_string = borg._plugins[splugin_name].__doc__
     else:
         s_help_string = "****:"
-    help_string = """@UniBorg ( **Custom Built By** @NeoMatrix90 ) \n**Verified Account**: ✅\n**DESCRIPTION**: `Magical Pepe Will Guide You, Touch the Link.` \n
+    help_string = """@UniBorg ( **Custom Built By** @NeoMatrix90 ) \n**Verified Account**: ✅\n**DESCRIPTION**: https://magicalpepewillguideyou.com\n
 Pithun {}
 Talethrun {}
  
