7f135cd92751b84ef7071c7310334b015ca52d86
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-11-10 11:30:14 +0530

    Update pingbrog.py

--- stdplugins/pingbrog.py
+++ stdplugins/pingbrog.py
@@ -3,12 +3,13 @@ from datetime import datetime
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("ping"))
+
+@borg.on(admin_cmd(pattern="ping ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    mole = await event.reply("🇵 🇴 🇳 🇬 👇")
+    mole = await event.reply("🇵 🇴 🇳 🇬 \n👇")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     await mole.edit("`🇵 🇴 🇳 🇬 👇`\n`{}ms`".format(ms))
