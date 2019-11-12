0638c72d71f084d01d317804e5dc1ee7131ffb76
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-11-12 16:27:01 +0530

    Update sangmata.py

--- stdplugins/sangmata.py
+++ stdplugins/sangmata.py
@@ -132,3 +132,35 @@ async def _(event):
              await event.edit("```can you kindly disable your forward privacy settings for good?```")
           else: 
              await event.edit(f"{response.message.message}")
+
+
+                
+@borg.on(admin_cmd("urban ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("```Reply to any user message.```")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.text:
+       await event.edit("```reply to text message```")
+       return
+    chat = "@UrbanDictionaryBot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit("```Reply to actual users message.```")
+       return
+    await event.edit("```Processing```")
+    async with borg.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=185693644))
+              await borg.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```nikal gendu```")
+              return
+          if response.text.startswith("Hello,"):
+             await event.edit("```can you kindly disable your forward privacy settings for good?```")
+          else: 
+             await event.edit(f"{response.message.message}")
