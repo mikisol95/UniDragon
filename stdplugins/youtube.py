4316c264d9311ce0bfb55c51972b407f7e8b6c7f
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2019-12-04 08:50:39 +0530

    Derp...

--- stdplugins/youtube.py
+++ stdplugins/youtube.py
@@ -18,7 +18,6 @@ from sample_config import Config
 
 
 @borg.on(events.NewMessage(pattern=r"\.youtube search (.*)", outgoing=True))
-@borg.on(events.MessageEdited(pattern=r"\.youtube search (.*)", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
