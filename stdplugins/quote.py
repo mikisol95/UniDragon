96724a183481f5b90458d803a26e41a8b629d5ef
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-01-08 18:26:15 +0530

    Update and rename quotes.py to quote.py

--- stdplugins/quote.py
+++ stdplugins/quote.py
@@ -2,7 +2,7 @@ import random
 import requests
 from uniborg.util import admin_cmd
 
-@borg.on(admin_cmd(pattern="quote ?(.*)"))
+@borg.on(admin_cmd(pattern="hquote ?(.*)"))
 async def quote_search(event):
     if event.fwd_from:
         return
