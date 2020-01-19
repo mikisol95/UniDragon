d627d04091f3fde98fab9c4b9a97624ed7c5791f
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-01-19 14:19:31 +0530

    Update chu.py

--- stdplugins/chu.py
+++ stdplugins/chu.py
@@ -1,31 +1,17 @@
 """COMMAND : .chu"""
 
 from telethon import events
-
 import asyncio
 
-
-
-
-
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
-
 async def _(event):
-
     if event.fwd_from:
-
         return
-
     animation_interval = 0.3
-
     animation_ttl = range(0, 30)
-
     input_str = event.pattern_match.group(1)
-
     if input_str == "chu":
-
         await event.edit(input_str)
-
         animation_chars = [
 
             "🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎\n🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎\n🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎\n🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎\n🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎🔴🔵🌕♓♎⛎",
@@ -90,7 +76,5 @@ async def _(event):
         ]
 
         for i in animation_ttl:
-
             await asyncio.sleep(animation_interval)
-
             await event.edit(animation_chars[i % 30])
