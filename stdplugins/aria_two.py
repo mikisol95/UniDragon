87dd9494053bf9c96a5eff5b9e1fdeec5565b95b
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-01-19 12:39:50 +0530

    Update aria_two.py

--- stdplugins/aria_two.py
+++ stdplugins/aria_two.py
@@ -13,7 +13,9 @@ import aria2p
 from telethon import events
 import asyncio
 import os
-
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.WARN)
 
 cmd = "aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800  --max-connection-per-server=10 --rpc-max-request-size=1024M --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 --daemon=true --allow-overwrite=true"
 EDIT_SLEEP_TIME_OUT = 5
@@ -28,7 +30,7 @@ aria2 = aria2p.API(
 	)
 
 
-@borg.on(events.NewMessage(pattern=r"\.imagnet", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.magnet", outgoing=True))
 async def magnet_download(event):
 	if event.fwd_from:
 		return
@@ -52,7 +54,7 @@ async def magnet_download(event):
 	
 
 
-@borg.on(events.NewMessage(pattern=r"\.itor", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.tor", outgoing=True))
 async def torrent_download(event):
 	if event.fwd_from:
 		return
@@ -68,7 +70,7 @@ async def torrent_download(event):
 	gid = download.gid
 	await progress_status(gid=gid,event=event,previous=None)
 
-@borg.on(events.NewMessage(pattern=r"\.iurl", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.url", outgoing=True))
 async def magnet_download(event):
 	if event.fwd_from:
 		return
@@ -88,7 +90,7 @@ async def magnet_download(event):
 		new_gid = await check_metadata(gid)
 		await progress_status(gid=new_gid,event=event,previous=None)
 
-@borg.on(events.NewMessage(pattern=r"\.iariaRM", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.ariaRM", outgoing=True))
 async def remove_all(event):
 	if event.fwd_from:
 		return
@@ -101,7 +103,7 @@ async def remove_all(event):
 		os.system("aria2p remove-all")
 	await event.edit("`Removed All Downloads.`")  
 
-@borg.on(events.NewMessage(pattern=r"\.ishow", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.show", outgoing=True))
 async def show_all(event):
 	if event.fwd_from:
 		return
