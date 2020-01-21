a6afc8571d71448bcedb92faa8dea78e4f4398ad
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-01-21 12:58:52 +0530

    Update bigf.py

--- stdplugins/bigf.py
+++ stdplugins/bigf.py
@@ -1,24 +1,14 @@
 """command: .kk"""
 """By @Grandpaa_please """
 
-
 from telethon import events
-
-import asyncio
 import random
 import logging
 
-
-
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
-
 async def _(event):
-
            input_str = event.pattern_match.group(1)
-           if input_str == "kk":
-
-                 
-             
+           if input_str == "kk":                              
                  r = random.randint(0, 3)
                  logger.debug(r)
                  if r == 0:
