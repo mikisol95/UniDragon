bd0742595a103d28f614eb62033c5b33bcfc1a26
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-01-21 13:51:26 +0530

    Uploaded New Plugin

---
+++ stdplugins/list_user_names_reserved_by_me.py
@@ -0,0 +1,14 @@
+# For @UniBorg
+# (c) Shrimadhav U K
+
+from telethon import events, functions, types
+
+@borg.on(events.NewMessage(pattern=r"\-listnames", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    result = await borg(functions.channels.GetAdminedPublicChannelsRequest())
+    output_str = ""
+    for channel_obj in result.chats:
+        output_str += f"- {channel_obj.title} @{channel_obj.username} \n"
+    await event.edit(output_str)
