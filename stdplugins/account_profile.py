33f272e14b7d60b1b54f995d9d086dbd4e4301ee
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-01-22 03:09:44 +0530

    Uploaded New Plugin

---
+++ stdplugins/account_profile.py
@@ -0,0 +1,125 @@
+"""Profile Updation Commands
+.pbio <Bio>
+.pname <Name>
+.ppic
+.delpfp <number or all> 
+to delete ur profile pic.
+.username <name>
+to change ur username."""
+import os
+from telethon.tl import functions
+from uniborg.util import admin_cmd
+from telethon.errors import ImageProcessFailedError, PhotoCropSizeSmallError
+from telethon.errors.rpcerrorlist import (PhotoExtInvalidError,
+                                          UsernameOccupiedError)
+from telethon.tl.functions.account import (UpdateProfileRequest,
+                                           UpdateUsernameRequest)
+from telethon.tl.functions.photos import (DeletePhotosRequest,
+                                          GetUserPhotosRequest,
+                                          UploadProfilePhotoRequest)
+from telethon.tl.types import InputPhoto, MessageMediaPhoto, User, Chat, Channel
+import logging
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+                    
+@borg.on(admin_cmd(pattern="pbio (.*)"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    bio = event.pattern_match.group(1)
+    try:
+        await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
+            about=bio
+        ))
+        await event.edit("Succesfully changed my profile bio")
+    except Exception as e:  # pylint:disable=C0103,W0703
+        await event.edit(str(e))
+
+
+@borg.on(admin_cmd(pattern="pname ((.|\n)*)"))  # pylint:disable=E0602,W0703
+async def _(event):
+    if event.fwd_from:
+        return
+    names = event.pattern_match.group(1)
+    first_name = names
+    last_name = ""
+    if  "\\n" in names:
+        first_name, last_name = names.split("\\n", 1)
+    try:
+        await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
+            first_name=first_name,
+            last_name=last_name
+        ))
+        await event.edit("My name was changed successfully")
+    except Exception as e:  # pylint:disable=C0103,W0703
+        await event.edit(str(e))
+
+
+@borg.on(admin_cmd(pattern="ppic"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    reply_message = await event.get_reply_message()
+    await event.edit("Downloading Profile Picture to my local ...")
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):  # pylint:disable=E0602
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)  # pylint:disable=E0602
+    photo = None
+    try:
+        photo = await borg.download_media(  # pylint:disable=E0602
+            reply_message,
+            Config.TMP_DOWNLOAD_DIRECTORY  # pylint:disable=E0602
+        )
+    except Exception as e:  # pylint:disable=C0103,W0703
+        await event.edit(str(e))
+    else:
+        if photo:
+            await event.edit("now, Uploading to @Telegram ...")
+            file = await borg.upload_file(photo)  # pylint:disable=E0602
+            try:
+                await borg(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
+                    file
+                ))
+            except Exception as e:  # pylint:disable=C0103,W0703
+                await event.edit(str(e))
+            else:
+                await event.edit("My profile picture was succesfully changed")
+    try:
+        os.remove(photo)
+    except Exception as e:  # pylint:disable=C0103,W0703
+        logger.warn(str(e))  # pylint:disable=E0602
+
+@borg.on(admin_cmd(pattern="delpfp ?(.*)"))
+async def remove_profilepic(delpfp):
+    """ For .delpfp command, delete your current profile picture in Telegram. """
+    group = delpfp.text[8:]
+    if group == 'all':
+        lim = 0
+    elif group.isdigit():
+        lim = int(group)
+    else:
+        lim = 1
+
+    pfplist = await delpfp.client(
+        GetUserPhotosRequest(user_id=delpfp.from_id,
+                             offset=0,
+                             max_id=0,
+                             limit=lim))
+    input_photos = []
+    for sep in pfplist.photos:
+        input_photos.append(
+            InputPhoto(id=sep.id,
+                       access_hash=sep.access_hash,
+                       file_reference=sep.file_reference))
+    await delpfp.client(DeletePhotosRequest(id=input_photos))
+    await delpfp.edit(
+        f"`Successfully deleted {len(input_photos)} profile picture(s).`")
+ 
+@borg.on(admin_cmd(pattern="username ?(.*)"))
+async def update_username(username):
+    """ For .username command, set a new username in Telegram. """
+    newusername = username.pattern_match.group(1)
+    try:
+        await username.client(UpdateUsernameRequest(newusername))
+        await username.edit("```Your username was succesfully changed.```")
+    except UsernameOccupiedError:
+        await username.edit("```This username is already taken by a faking Nibba.```")        
