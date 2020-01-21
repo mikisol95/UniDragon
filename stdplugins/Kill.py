5e3f23338fc09cdb16d938916c927147321138f4
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-01-22 02:53:21 +0530

    Cu

--- stdplugins/Kill.py
+++ stdplugins/Kill.py
@@ -1,32 +1,18 @@
 """COMMAND .kill"""
 
 from telethon import events
-
 import asyncio
-
-
-
-
 
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
-
     if event.fwd_from:
-
         return
-
     animation_interval = 30
-    
-
-    animation_ttl = range(0, 100)
-
+    animation_ttl = range(0, 88)
     input_str = event.pattern_match.group(1)
-
     if input_str == "kill":
-
         await event.edit(input_str)
-
         animation_chars = [
 
             "Ｆｉｉｉｉｉｒｅ",
@@ -44,6 +30,4 @@ async def _(event):
         ]
 
         for i in animation_ttl:
-
-
-            await event.edit(animation_chars[i % 100])
+            await event.edit(animation_chars[i % 88])
