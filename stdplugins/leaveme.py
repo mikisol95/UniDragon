7c83027c449e68649aac82129dc27192615c9319
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-01-24 11:35:12 +0530

    Hs

--- stdplugins/leaveme.py
+++ stdplugins/leaveme.py
@@ -8,7 +8,6 @@ from telethon.tl.functions.channels import LeaveChannelRequest
 from uniborg.util import admin_cmd
 import time
 
-
 @borg.on(admin_cmd("leave", outgoing=True))
 async def leave(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
@@ -17,4 +16,4 @@ async def leave(e):
         if '-' in str(e.chat_id):
             await borg(LeaveChannelRequest(e.chat_id))
         else:
-            await e.edit('`Sir This is Not A Chat`')
+            await e.edit('`Sur This is Not A Chat`')
