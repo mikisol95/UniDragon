54581f36a453007d3dacdd9b6bb81dafd6606df1
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-01-24 10:35:23 +0530

    Update execmod.py

--- stdplugins/execmod.py
+++ stdplugins/execmod.py
@@ -750,7 +750,7 @@ async def _(event):
     PROCESS_RUN_TIME = 100
 #    dirname = event.pattern_match.group(1)
 #    tempdir = "localdir"
-    cmd = "pytuneteller pisces --today"
+    cmd = "pytuneteller gemini --today"
 #    if dirname == tempdir:
 	
     eply_to_id = event.message.id
