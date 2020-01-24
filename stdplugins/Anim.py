3b32462b0320897093ccceae54d64da9fa73811c
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-01-24 10:59:12 +0530

    Update Anim.py

--- stdplugins/Anim.py
+++ stdplugins/Anim.py
@@ -5,7 +5,8 @@ Available Commands:
 .eye
 .netflix
 .cry
-.🍎 to print a stack of 🍎"""
+.🍎 to print a stack of 🍎
+.bombs to RIP People 😈"""
 
 from telethon import events
 import asyncio
@@ -289,3 +290,29 @@ async def _(event):
             msg += s+"\n"
             await event.edit(msg)
             await asyncio.sleep(0.5)
+
+@borg.on(events.NewMessage(pattern=r"\.bombs", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("💣💣💣💣 \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n💣💣💣💣 \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n💣💣💣💣 \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n💣💣💣💣 \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n💣💣💣💣 \n")
+    await asyncio.sleep(1)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n💥💥💥💥 \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n💥💥💥💥 \n💥💥💥💥 \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n😵😵😵😵 \n")
+    await asyncio.sleep(0.5)
+    await event.edit("`RIP PLOX...`")
+    await asyncio.sleep(2)
+    await event.delete()
