cf1398c08190ff131a7e1c3f52deb09ce405b984
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-01-25 02:13:49 +0530

    Update shoutadmin.py

--- stdplugins/shoutadmin.py
+++ stdplugins/shoutadmin.py
@@ -1,68 +1,19 @@
 """Emoji
-
-
-
 Available Commands:
-
-
 .admem"""
 
-
-
 from telethon import events
-
-
-
 import asyncio
 
-
-
-
-
-
-
-
-
-
-
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
-
-
-
 async def _(event):
-
-
-
     if event.fwd_from:
-
-
-
         return
-
-
-
     animation_interval = 1
-
-    
-
-
-
-    animation_ttl = range(0, 103)
-
-
-
+    animation_ttl = range(0, 96)
     input_str = event.pattern_match.group(1)
-
-
-
     if input_str == "admem":
-
-
-
         await event.edit(input_str)
-
-
-
         animation_chars = [
 
 
@@ -95,12 +46,5 @@ async def _(event):
 
         ]
 
-
-
         for i in animation_ttl:
-
-
-
-
-
-            await event.edit(animation_chars[i % 103])
+            await event.edit(animation_chars[i % 96])
