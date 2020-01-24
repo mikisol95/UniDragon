9db240fece4bad322bae0c5b0c908097f79c8c61
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-01-24 10:45:09 +0530

    Update Kirito.py

--- stdplugins/Kirito.py
+++ stdplugins/Kirito.py
@@ -3,7 +3,8 @@ Available Commands:
 .kirito
 .nikita
 .eye
-.netflix"""
+.netflix
+.cry"""
 
 from telethon import events
 
@@ -269,3 +270,64 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 9])
+            
+            
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 1
+
+    
+
+    animation_ttl = range(0, 103)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "cry":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            ";__",
+
+            ";___",
+
+            ";____",
+
+            ";_____",
+
+            ";______",
+
+            ";_______",
+
+            ";________",
+
+            ";__________",
+
+            ";____________",
+
+            ";______________",
+
+            ";________________",
+
+            ";__________________",
+
+            ";____________________",
+
+            ";______________________",
+
+            ";________________________",
+
+            ";_________________________;"
+
+        ]
+
+        for i in animation_ttl:
+
+            await event.edit(animation_chars[i % 15])
