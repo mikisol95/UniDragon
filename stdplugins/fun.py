417fd423584b349f77020f90094f241ed124161a
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-02-03 22:56:40 +0530

    Update fun.py

--- stdplugins/fun.py
+++ stdplugins/fun.py
@@ -1,4 +1,4 @@
-"""COMMAND : .runs , .metoo , .rape , .insult , .pro , .abuse , .gey"""
+"""COMMAND : .runs , .metoo , .rape , .inkut , .pro , .abuse , .gey"""
 
 from telethon import events
 import random, re
@@ -209,7 +209,7 @@ async def _(event):
     await event.edit(reply_text)
 			  
                           
-@borg.on(admin_cmd("insult($)"))
+@borg.on(admin_cmd("inkut($)"))
 async def _(event):
     if event.fwd_from:
          return
