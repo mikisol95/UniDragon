0a1d91c40dc627ed4f20a662defef89f1ba5364d
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-02-10 20:24:35 +0530

    Update stt.py

--- stdplugins/stt.py
+++ stdplugins/stt.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="stt (.*)"))
+@borg.on(admin_cmd(pattern="stt ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
@@ -15,18 +15,18 @@ async def _(event):
     input_str = event.pattern_match.group(1)
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    await event.edit("Downloading to my ravana69/pornhub server for analysis 🙇")
+    await event.edit("`Downloading to My prono69/PepeBot Server for Analysis` 🙇")
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
         required_file_name = await borg.download_media(
             previous_message,
             Config.TMP_DOWNLOAD_DIRECTORY
         )
-        lan = input_str
+        lan = input_str or "eng"
         if Config.IBM_WATSON_CRED_URL is None or Config.IBM_WATSON_CRED_PASSWORD is None:
             await event.edit("You need to set the required ENV variables for this module. \nModule stopping")
         else:
-            await event.edit("Starting analysis, using IBM WatSon Speech To Text")
+            await event.edit("`Starting analysis, Using IBM WatSon Speech To Text`")
             headers = {
                 "Content-Type": previous_message.media.document.mime_type,
             }
@@ -45,12 +45,12 @@ async def _(event):
                 transcript_confidence = ""
                 for alternative in results:
                     alternatives = alternative["alternatives"][0]
-                    transcript_response += " " + str(alternatives["transcript"]) + " + "
+                    transcript_response += " " + str(alternatives["transcript"])
                     transcript_confidence += " " + str(alternatives["confidence"]) + " + "
                 end = datetime.now()
                 ms = (end - start).seconds
                 if transcript_response != "":
-                    string_to_show = "Language: `{}`\nTRANSCRIPT: `{}`\nTime Taken: {} seconds\nConfidence: `{}`".format(lan, transcript_response, ms, transcript_confidence)
+                    string_to_show = "Language: `{}`\n**TRANSCRIPT:** `{}`\n\nTime Taken: {} seconds\nConfidence: `{}`".format(lan, transcript_response, ms, transcript_confidence)
                 else:
                     string_to_show = "Language: `{}`\nTime Taken: {} seconds\n**No Results Found**".format(lan, ms)
                 await event.edit(string_to_show)
