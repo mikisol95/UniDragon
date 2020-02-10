b214884e3d839a557349fe62e6cabab3b3781ed2
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-02-10 20:31:43 +0530

    Uploaded New Plugin

---
+++ stdplugins/translate.py
@@ -0,0 +1,44 @@
+""" Google Translate
+Available Commands:
+.tr LanguageCode as reply to a message
+.tr LangaugeCode | text to translate"""
+
+import emoji
+from googletrans import LANGUAGES, Translator
+from uniborg.util import admin_cmd
+
+
+@borg.on(admin_cmd(pattern="tr ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        text = previous_message.message
+        lan = input_str or "en"
+    elif "|" in input_str:
+        lan, text = input_str.split("|")
+    else:
+        await event.edit("`.tr LanguageCode` as reply to a message")
+        return
+    text = emoji.demojize(text.strip())
+    lan = lan.strip()
+    translator = Translator()
+    try:
+        translated = translator.translate(text, dest=lan)
+        after_tr_text = translated.text
+        # TODO: emojify the :
+        # either here, or before translation
+        source_lan = LANGUAGES[f'{translated.src.lower()}']
+        transl_lan = LANGUAGES[f'{translated.dest.lower()}']
+        output_str = """**Text:** {}\n**Detected Language:** `{}`\n\n**TRANSLATED To** `{}`:\n{}
+""".format(
+            previous_message.message,
+            source_lan.title(),
+            transl_lan.title(),
+            after_tr_text
+        )
+        await event.edit(output_str)
+    except Exception as exc:
+        await event.edit(str(exc))
