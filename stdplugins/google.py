e9e5990227b1e21506d2cdbda8aa122b8dc679b7
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-02-19 09:38:53 +0530

    Update google.py

--- stdplugins/google.py
+++ stdplugins/google.py
@@ -133,7 +133,7 @@ async def _(event):
         OUTPUT_STR = """{img_size}
 *Possible Related Search*: <a href="{prs_url}">{prs_text}</a>
 
-More Info: Open this <a href="{the_location}">LINK</a> in {ms} seconds""".format(**locals())
+More Info: Open This <a href="{the_location}">LINK</a> in {ms} seconds""".format(**locals())
     await event.edit(OUTPUT_STR, parse_mode="HTML", link_preview=False)
 
 
