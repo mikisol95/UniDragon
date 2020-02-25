d87272113bd78242e4295ea22427fae40eecb011
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-02-25 14:13:31 +0530

    Update download_v2.py

--- stdplugins/download_v2.py
+++ stdplugins/download_v2.py
@@ -10,7 +10,7 @@ import math
 import os
 import time
 from datetime import datetime
-from bin.fasttelethon import download_file
+from bin.fastelethon import download_file
 import aiohttp
 from pySmartDL import SmartDL
 from telethon import events
