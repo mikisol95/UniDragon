83d675a31a2487b8c39bb532a6505a46fd0fb11d
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-02-25 21:05:20 +0530

    Ok fixed

--- stdplugins/misc.py
+++ stdplugins/misc.py
@@ -4,6 +4,7 @@ from asyncio import create_subprocess_shell as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
 from platform import python_version, uname
 from os import remove
+from telethon import version
 
 """Type:
 \n.permalink
