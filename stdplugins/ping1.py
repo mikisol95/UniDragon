b8324edaaa56b2bbf77211a53ea8089004ff9833
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-06-03 10:11:29 +0530

    Uploaded New Plugin

---
+++ stdplugins/ping1.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from uniborg.util import admin_cmd, edit_or_reply
+
+
+@borg.on(admin_cmd(pattern="ping", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    ed = await edit_or_reply(event, event.from_id, "...")
+    start = datetime.now()
+    await ed.edit("Pong!")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await ed.edit("**Pong!**\n`{}` __ms__".format(ms))
