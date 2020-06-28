1d395a9e04665f289189802f742ebef21c61b327
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-06-28 23:08:16 +0530

    Introducing hash, animelist and nekos Modules
    Introducing hash, animelist and nekos Modules for PepeBot. Please give proper credits if you want to kang them.
    I made nekos and animelist modules with Hard work :) me nub that's why. So Give Proper Credits.
    Enjoy Them. Bye

---
+++ stdplugins/hash.py
@@ -0,0 +1,84 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot module containing hash and encode/decode commands.
+\nPorted By- [NIKITA](https://t.me/kirito6969) #LazyAF_Geng 🔥
+"""
+
+from subprocess import PIPE
+from subprocess import run as runapp
+import pybase64
+from uniborg.util import admin_cmd
+from uniborg import MODULE, SYNTAX
+MODULE.append("hash")
+
+@borg.on(admin_cmd(pattern="hash ?(.*)"))
+async def gethash(hash_q):
+    """ For .hash command, find the md5, sha1, sha256, sha512 of the string. """
+    hashtxt_ = hash_q.pattern_match.group(1)
+    if not hashtxt_:
+    	get = await hash_q.get_reply_message()
+    	hashtxt_ = get.text
+    else:
+    	await hash_q.edit("Brah.. Gib me Something")
+    	return
+    	
+    hashtxt = open("hashdis.txt", "w+")
+    hashtxt.write(hashtxt_)
+    hashtxt.close()
+    md5 = runapp(["md5sum", "hashdis.txt"], stdout=PIPE)
+    md5 = md5.stdout.decode()
+    sha1 = runapp(["sha1sum", "hashdis.txt"], stdout=PIPE)
+    sha1 = sha1.stdout.decode()
+    sha256 = runapp(["sha256sum", "hashdis.txt"], stdout=PIPE)
+    sha256 = sha256.stdout.decode()
+    sha512 = runapp(["sha512sum", "hashdis.txt"], stdout=PIPE)
+    runapp(["rm", "hashdis.txt"], stdout=PIPE)
+    sha512 = sha512.stdout.decode()
+    ans = ("Text: `" + hashtxt_ + "`\nMD5: `" + md5 + "`SHA1: `" + sha1 +
+           "`SHA256: `" + sha256 + "`SHA512: `" + sha512[:-1] + "`")
+    if len(ans) > 4096:
+        hashfile = open("hashes.txt", "w+")
+        hashfile.write(ans)
+        hashfile.close()
+        await hash_q.client.send_file(
+            hash_q.chat_id,
+            "hashes.txt",
+            reply_to=hash_q.id,
+            caption="`It's too big, sending a text file instead. `")
+        runapp(["rm", "hashes.txt"], stdout=PIPE)
+    else:
+        await hash_q.reply(ans)
+
+
+@borg.on(admin_cmd(pattern="base (en|de) ?(.*)"))
+async def endecrypt(query):
+    """ For .base64 command, find the base64 encoding of the given string. """
+    input_str = query.pattern_match.group(2)
+    if not input_str:
+    	get = await query.get_reply_message()
+    	input_str = get.text
+    else:
+    	await query.edit("Uffff.. Sar Gib me something")
+    	return
+    if query.pattern_match.group(1) == "en":
+        lething = str(
+            pybase64.b64encode(bytes(input_str,
+                                     "utf-8")))[2:]
+        await query.reply("Encoded: `" + lething[:-1] + "`")
+    else:
+        lething = str(
+            pybase64.b64decode(bytes(input_str, "utf-8"),
+                               validate=True))[2:]
+        await query.reply("Decoded: `" + lething[:-1] + "`")
+
+
+SYNTAX.update({
+    "hash":
+    "`.base64`\
+    \n`Usage: Find the base64 encoding of the given string.`\
+    \n\n`.hash`\
+    \n`Usage: Find the md5, sha1, sha256, sha512 of the string when written into a txt file.`"
+})
---
+++ stdplugins/nekos.py
@@ -0,0 +1,113 @@
+# #LazyAF_Geng 🔥
+
+"""NEKOS MODULE FOR PEPEBOT
+\nPlugin Made by [NIKITA](https://t.me/kirito6969)
+Cmds = `.nk <argument from POSSIBLE list>`
+\nPOSSIBLE = [
+        'feet', 'yuri', 'trap', 'futanari', 'hololewd', 'lewdkemo',
+        'solog', 'feetg', 'cum', 'erokemo', 'les', 'wallpaper', 'lewdk',
+        'ngif', 'tickle', 'lewd', 'feed', 'gecg', 'eroyuri', 'eron',
+        'cum_jpg', 'bj', 'nsfw_neko_gif', 'solo', 'kemonomimi', 'nsfw_avatar',
+        'gasm', 'poke', 'anal', 'slap', 'hentai', 'avatar', 'erofeet', 'holo',
+        'keta', 'blowjob', 'pussy', 'tits', 'holoero', 'lizard', 'pussy_jpg',
+        'pwankg', 'classic', 'kuni', 'waifu', 'pat', '8ball', 'kiss', 'femdom',
+        'neko', 'spank', 'cuddle', 'erok', 'fox_girl', 'boobs', 'random_hentai_gif',
+        'smallboobs', 'hug', 'ero', 'smug', 'goose', 'baka', 'woof'
+    ]
+\n`.dva`\n`.nsfw`\n`.cat`\n`.lewdn`\n`.why`\n`.gasm`\n`.ifu`\n`.fact`\n`.tcat`
+\n\nNSFW PLUGIN. BE AWARE 🔴
+\n\n**DON'T EVEN TRY TO TO CHANGE CREDITS**'
+"""
+
+import nekos
+from uniborg.util import admin_cmd
+import json
+import requests
+from PIL import Image
+import os
+from uniborg import MODULE, SYNTAX
+
+MODULE.append("nekos")
+
+@borg.on(admin_cmd(pattern="nk ?(.*)"))
+async def _(event):
+	hmm = event.pattern_match.group(1)
+	if not hmm:
+		await event.edit("`Bruh.. What I am supposed to do!`")
+		return
+	if nekos.errors.InvalidArgument:
+	    await event.edit("`You haven't added any valid arguments`")
+	    return
+	await event.edit("`Processing...Nekos`")
+	target = nekos.img(f'{hmm}')
+	await event.client.send_file(event.chat_id, file=target, caption=f"{hmm}")
+
+@borg.on(admin_cmd(pattern="dva"))
+async def dva(event):
+    nsfw = requests.get("https://api.computerfreaker.cf/v1/dva").json()
+    url = nsfw.get("url")
+    if not url:
+    	await event.edit("`uuuf.. No URL found from the API`")
+    	return
+    await event.client.send_file(event.chat_id, file=url)
+    
+@borg.on(admin_cmd(pattern="nsfw"))    
+async def avatarlewd(event):
+    target = 'nsfw_avatar'
+    with open("temp.png", "wb") as f:
+        f.write(requests.get(nekos.img(target)).content)
+    img = Image.open("temp.png")
+    img.save("temp.webp", "webp")
+    await event.client.send_file(event.chat_id, file=open("temp.webp", "rb"))
+    os.remove("temp.webp")
+    
+@borg.on(admin_cmd(pattern="cat"))
+async def _(event):
+	target = nekos.cat()
+	await event.edit("Finding ur ket...🌚")
+	await event.client.send_file(event.chat_id, file=target)
+
+@borg.on(admin_cmd(pattern="lewdn"))
+async def dva(event):
+    nsfw = requests.get("https://nekos.life/api/lewd/neko").json()
+    url = nsfw.get("neko")
+    if not url:
+    	await event.edit("`uuuf.. No NEKO found from the API`")
+    	return
+    await event.client.send_file(event.chat_id, file=url)
+
+@borg.on(admin_cmd(pattern="why"))
+async def _(event):
+	target = nekos.why()
+	await event.edit(target)
+
+@borg.on(admin_cmd(pattern="gasm"))
+async def gasm(event):
+    target = 'gasm'
+    with open("temp.png", "wb") as f:
+        f.write(requests.get(nekos.img(target)).content)
+    img = Image.open("temp.png")
+    img.save("temp.webp", "webp")
+    await event.client.send_file(event.chat_id, file=open("temp.webp", "rb"))
+    os.remove("temp.webp")
+
+@borg.on(admin_cmd(pattern="ifu"))
+async def waifu(event):
+    target = 'waifu'
+    with open("temp.png", "wb") as f:
+        f.write(requests.get(nekos.img(target)).content)
+    img = Image.open("temp.png")
+    img.save("temp.webp", "webp")
+    await event.client.send_file(event.chat_id, file=open("temp.webp", "rb"))
+    os.remove("temp.webp")
+ 
+@borg.on(admin_cmd(pattern="fact"))
+async def _(event):
+	target= nekos.fact()
+	await event.edit(target)
+
+@borg.on(admin_cmd(pattern="tcat"))
+async def _(event):
+	target = nekos.textcat()
+	await event.edit(target)
+	
