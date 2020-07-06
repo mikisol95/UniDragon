496cf2a3bc221cf55afef60500f5b5b1c3e10811
prono69 <faltu9851@gmail.com>
2020-07-06 14:23:43 +0530

    f 🌚🤣

--- userbot/anime.py
+++ userbot/anime.py
@@ -51,9 +51,9 @@ def get_anime_manga(mal_id, search_type, _user_id):
         result = jikan.anime(mal_id)
         trailer = result['trailer_url']
         if trailer:
-        	LOL = "<a href='{trailer}'>Trailer</a>"
+        	LOL = f"<a href='{trailer}'>Trailer</a>"
         else:
-        	LOL = "Nu Trailer Available"
+        	LOL = "<code>Nu Trailer Available</code>"
         image = getBannerLink(mal_id)
         studio_string = ', '.join(studio_info['name'] for studio_info in result['studios'])
         producer_string = ', '.join(producer_info['name'] for producer_info in result['producers'])
@@ -133,10 +133,12 @@ def get_poster(query):
 def post_to_telegraph(anime_title, html_format_content):
     post_client = TelegraphPoster(use_api=True)
     auth_name = "@LazyAF_Pepe"
-    post_client.create_api_token(auth_name, "@LazyAF_Pepe", "https://telegram.dog/LazyAF_Pepe")
+    bish = "https://t.me/LazyAF_Pepe"
+    post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=anime_title,
         author=auth_name,
+        author_url=bish,
         text=html_format_content
     )
     return post_page['url']
