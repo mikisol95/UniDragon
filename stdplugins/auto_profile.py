7abd098dea3efcf3751f08860ff17c5a13bbe38f
Kaleb <65592198+Kaleb1370@users.noreply.github.com>
2020-08-14 16:18:10 +0300

    Update auto_profile.py

--- stdplugins/auto_profile.py
+++ stdplugins/auto_profile.py
@@ -45,7 +45,7 @@ async def _(event):
     while True:
         DM = time.strftime("%d.%m.%y")
         HM = time.strftime("%H:%M")
-        name = f"{HM} </ マヒ・イト >/{DM}"
+        name = f"{HM}</ マヒ・イト >/{DM}"
         logger.info(name)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
